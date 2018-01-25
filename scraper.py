36957bffd147e73963e5fdb136abaf252f061277
majupi <35702445+majupi@users.noreply.github.com>
2018-01-25 15:56:20 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,16 +15,16 @@ record = {}
 # # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
 names = root.cssselect("td div a")
-for i in names:
+#for i in names:
   print i.attrib['href']
   print i.text.encode("ascii", "ignore")
   record['link'] = i.attrib['href']
   record['name'] = i.text.encode("ascii", "ignore")
   print record
-  scraperwiki.sqlite.save(unique_keys=['link','name'], data=record) # this saves the data in a way you can download it.
-  
+  scraperwiki.sqlite.save(unique_keys=['link'], data=record) # this saves the data in a way you can download it.
 
-  
+age = root.cssselect("td div[2]")
+print age
 
   
   

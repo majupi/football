a00f505336f02a3d4d6d302c2e5d054ac4a446d4
majupi <35702445+majupi@users.noreply.github.com>
2018-01-25 15:43:36 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -17,6 +17,7 @@ root = lxml.html.fromstring(html)
 names = root.cssselect("td div a")
 for i in names:
   print i.attrib['href']
+  print i.text.encode("ascii", "ignore")
   record['link'] = i.attrib['href']
   print record
   scraperwiki.sqlite.save(unique_keys=['link'], data=record) # this saves the data in a way you can download it.
